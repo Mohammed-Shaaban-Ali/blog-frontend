7715736eb65d7fe4deb50324a32980c44e10ce0e
Mohammed Shaaban <ms7500746@gnail.com>
2023-07-03 17:39:30 +0300

    first update in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home ss aa</div>;
+  return (
+    <section className="home">
+      <div className="home-hero-header">
+        <div className="home-hero-header-layout">
+          <h1 className="home-title">Welcome to Blog</h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
